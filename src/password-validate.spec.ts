336692dcb01be967643c3497cd09a2eea399c4f0
Ainoa <aimmolina2@gmail.com>
2026-02-01 13:45:25 +0100

    password validator ready

--- src/password-validate.spec.ts
+++ src/password-validate.spec.ts
@@ -15,4 +15,50 @@ describe("validarClave", () => {
 
     expect(validacion).toEqual(expected);
   });
+  it("debería retornar esValida: false para una clave sin mayúsculas", () => {
+    const nombreUsuario: string = "usuario01";
+    const clave: string = "clavesegura123!";
+    const validacion: ValidacionClave = validarClave(
+      nombreUsuario,
+      clave,
+      commonPasswords,
+    );
+    const expected: ValidacionClave = {
+      esValida: false,
+      error: "La clave debe de tener mayúsculas y minúsculas",
+    };
+
+    expect(validacion).toEqual(expected);
+  });
+  it("debería retornar esValida: false para una clave sin números", () => {
+    const nombreUsuario: string = "usuario01";
+    const clave: string = "ClaveSeguraABC!";
+    const validacion: ValidacionClave = validarClave(
+      nombreUsuario,
+      clave,
+      commonPasswords,
+    );
+    const expected: ValidacionClave = {
+      esValida: false,
+      error: "La clave debe de tener números",
+    };
+
+    expect(validacion).toEqual(expected);
+  });
+
+  it("debería retornar esValida: false para una clave que contiene el nombre de usuario", () => {
+    const nombreUsuario: string = "usuario01";
+    const clave: string = "ClaveUsuario01SeguraABC!";
+    const validacion: ValidacionClave = validarClave(
+      nombreUsuario,
+      clave,
+      commonPasswords,
+    );
+    const expected: ValidacionClave = {
+      esValida: false,
+      error: "La clave no debe tener el nombre del usuario",
+    };
+
+    expect(validacion).toEqual(expected);
+  });
 });
